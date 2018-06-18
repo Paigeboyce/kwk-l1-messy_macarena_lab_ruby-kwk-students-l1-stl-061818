94301c9087ca9a5d8c2e7a5301bccf1446128cca
Paigeboyce <boycepaige1@gmail.com>
2018-06-18 21:26:13 +0000

    automatically backed up by learn

--- macarena.rb
+++ macarena.rb
@@ -8,7 +8,7 @@ puts ""
 # Step 1: GOOD
 puts "Step 1: Right arm out in front of you, palm facing down."
 
-# Step 2: FIXME
+# Step 3: FIXME
 puts "Step 3: Turn your right palm up."
 
 # Step 3: FIXME
@@ -17,14 +17,14 @@ puts "Step 3: Place your right hand on your left shoulder."
 # Step 4: FIXME
 puts "Step 4: Turn your left palm up."
 
-# Step 5: FIXME
+# Step 7: FIXME
 puts "Step 7: Bring your right hand up and around to the back of your head."
 
-# Step 6: FIXME
+# Step 2: FIXME
 puts "Step 2: Left arm out in front of you, palm facing down."
 
-# Step 7: FIXME
+# Step 6: FIXME
 puts "Step 6: Place your left hand on your right shoulder."
 
 # Step 8: FIXME
-puts "Step 8: The Last Step"
+puts "Step 8: Bring your left hand up and around to the back of your head"
